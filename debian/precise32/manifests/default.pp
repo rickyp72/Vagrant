cf6136989358db4007e0fcb81c02a461e34150aa
Ricky Parkinson <ricky.parkinson@futurenet.com>
2013-08-06 10:05:08 +0100

    initial commit

---
+++ debian/precise32/manifests/default.pp
@@ -0,0 +1,41 @@
+exec { "apt-get update":
+  path => "/usr/bin",
+}
+package { "apache2":
+  ensure  => present,
+  require => Exec["apt-get update"],
+}
+service { "apache2":
+  ensure  => "running",
+  require => Package["apache2"],
+}
+# file { "/var/www/sample-webapp":
+#   ensure  => "link",
+#   target  => "/vagrant/sample-webapp",
+#   require => Package["apache2"],
+#   notify  => Service["apache2"],
+# }
+# package { "php5":
+# 	ensure => present,
+# }
+# package { "php5-fpm":
+# 	ensure => present,
+# 	require => Package["php5"],
+# }
+# 
+# package { "nginx":
+# 	ensure => present,
+# 
+# }
+# 
+# service { "nginx":
+# 	ensure => "running",
+# 	require => Package["nginx"],
+# }
+# 
+# file { "/usr/share/nginx/www/sample-webapp":
+#   ensure  => "link",
+#   target  => "/vagrant/sample-webapp",
+#   require => Package["nginx"],
+#   notify  => Service["nginx"],
+# }
